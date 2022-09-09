d83ebee27e2c19c4c5b850579d82422cf0062745
rajkumarV <rajkumarvdsr@gmail.com>
2022-09-09 18:39:47 +0530

    first commit

---
+++ Day1_SumOfNumbers.java
@@ -0,0 +1,15 @@
+package raj.ds;
+
+import java.util.Scanner;
+
+public class Day1_SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the value of a");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b9");
+        int b = sc.nextInt();
+        int c =a+b;
+        System.out.print("sum of a and b are "+c);
+    }
+}
---
+++ PatternStar.java
@@ -0,0 +1,13 @@
+package raj.ds;
+
+public class PatternStar {
+    public static void main(String[] args) {
+        int i,j,row=4;
+        for(i = 0; i<=row; i++){
+            for(j=0; j<i; j++){
+                System.out.print("x");
+            }
+            System.out.println();
+        }
+    }
+}
